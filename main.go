4e0307cd5e37f13504f3da19728f571a40cfa181
Jorge Gonzalez <jorg3.594@gmail.com>
2022-12-19 22:45:36 -0300

    Add Repository to main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	indexer "github.com/jorgeluis594/go_indexer/src"
 	"log"
 	"net/mail"
@@ -11,19 +10,36 @@ import (
 
 func main() {
 	path := "harris-s"
+	host := "http://localhost:4080"
+	username := "admin"
+	password := "Complexpass#123"
+	clientHttp := indexer.InitHttpClient(host, username, password)
+	repository := indexer.InitRepository(clientHttp)
 	emails, success := loadEmails(path)
-	if success {
-		fmt.Println(len(*emails))
+	if !success {
+		log.Fatal("No se pudieron cargar los emails del directorio: ", path)
+	}
+
+	for len(emails) > 0 {
+		var numberOfEmails int
+		if len(emails) < 1000 {
+			numberOfEmails = len(emails)
+		} else {
+			numberOfEmails = 1000
+		}
+		emailsToSend := emails[:numberOfEmails]
+		emails = emails[numberOfEmails:]
+		repository.PersistEmails(emailsToSend)
 	}
 }
 
-func loadEmails(path string) (*[]indexer.Mail, bool) {
+func loadEmails(path string) ([]indexer.Mail, bool) {
 	emails := make([]indexer.Mail, 0)
 	directory, err := indexer.InitDirectory("harris-s")
 
 	if err != nil {
 		log.Fatal("Error reading directory: ", path)
-		return &emails, false
+		return emails, false
 	}
 
 	for _, path := range directory.GetPaths() {
@@ -39,7 +55,7 @@ func loadEmails(path string) (*[]indexer.Mail, bool) {
 		emails = append(emails, *email)
 	}
 
-	return &emails, true
+	return emails, true
 }
 
 func readEmail(path string) (*mail.Message, bool) {
