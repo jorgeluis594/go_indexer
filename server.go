81f8df9e536c583a30feb9a6e32c700660c62d6f
Jorge Gonzalez <jorg3.594@gmail.com>
2022-12-21 20:38:31 -0300

    Add endpoint to search emails

--- server.go
+++ server.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/jorgeluis594/go_indexer/repository"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func main() {
+	zincRepository := initRepository()
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -21,8 +26,38 @@ func main() {
 		fmt.Fprintf(w, string(content))
 	})
 
+	r.Get("/api/search", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		q := r.URL.Query().Get("q")
+		page := r.URL.Query().Get("page")
+
+		if page == "" {
+			page = "1"
+		}
+		currentPage, err := strconv.Atoi(page)
+		if err != nil {
+			currentPage = 1
+		}
+		if q == "" {
+			json.NewEncoder(w).Encode(repository.SearchQuery{})
+		}
+
+		response := zincRepository.Search(q, currentPage)
+		json.NewEncoder(w).Encode(response)
+	})
+
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initRepository() repository.Repository {
+	host := flag.String("host", "", "host of Zinc Search client")
+	username := flag.String("username", "", "username of db")
+	password := flag.String("password", "", "password of db")
+	flag.Parse()
+
+	clientHttp := repository.InitHttpClient(*host, *username, *password)
+	return repository.InitRepository(clientHttp, "email_copy")
 }
